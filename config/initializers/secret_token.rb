ac7733e6661300d04f477ab7eb54d2cdf828ba37
Rubynator <sectronov+rubynator@gmail.com>
2012-05-16 10:27:29 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Micro::Application.config.secret_token = '5dc266aed73e61066158f620887531fa5c5546ead6fcd4a56d09d270cf0d052711397a421164438e88230c478bcf0c4a544b82af1dd22dc21aa3ee1e351f1701'
